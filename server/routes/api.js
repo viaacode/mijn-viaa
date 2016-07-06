d8e62cac17bde063980437f8539b55813308039a
Tjen Wellens <(none)>
2016-07-06 15:32:22 +0200

    fetch terrabytes and items from labs-archived

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,15 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
+// used in callback argument to return if successful
+// otherwise the error object/message is returned (not a falsy value)
+var SUCCESS = '';
 
 //region Dummy data
 var dummy = {
   global: {
-    tb: 42,
+    terabytes: 42,
     items: 2471,
     archive_growth: 446.12,
     registration_growth: 443.5
@@ -14,15 +18,15 @@ var dummy = {
 };
 
 function createDummyOrganisationsStats () {
-  var companies = new Array();
-  companies["vrt"] = {
-    tb: 33,
+  var companies = [];
+  companies["VRT"] = {
+    terabytes: 33,
     items: 234,
     archive_growth: 3333.12,
     registration_growth: 867.5
   };
   companies["plantentuin"] = {
-    tb: 11,
+    terabytes: 11,
     items: 64564,
     archive_growth: 456.12,
     registration_growth: 23.5
@@ -31,9 +35,38 @@ function createDummyOrganisationsStats () {
 }
 //endregion
 
-router.get('/stats/', function (req, res, next) {
-  res.json(dummy.global);
+function fetchStats (organisation, callback) {
 
+  var url = 'http://labs.viaa.be/api/v1/archived';
+
+  if (organisation) {
+    url += '?tenant=' + organisation;
+  }
+
+  request(url, function (error, response, body) {
+    if (error) return callback(error);
+    if (response.statusCode != 200) return callback('Statuscode: ' + response.statusCode);
+
+    console.log(obj);
+    var obj = parseStats(JSON.parse(body));
+    callback(SUCCESS, obj);
+  });
+}
+
+function parseStats (inputObject) {
+  return {
+    terabytes: inputObject.response.data.archived.terabytes,
+    items: inputObject.response.data.archived.all,
+    archive_growth: 111.12,
+    registration_growth: 111.5
+  };
+}
+
+router.get('/stats/', function (req, res, next) {
+  fetchStats(false, function (err, data) {
+    if (err) return next(err);
+    res.json(data);
+  });
 });
 
 // todo: auth
@@ -43,7 +76,10 @@ router.get('/stats/organisation/:organisationId', function (req, res, next) {
 
   if (!organisation) return next("Organisation '" + organisationId + "' does not exist");
 
-  res.json(organisation);
+  fetchStats(false, function (err, data) {
+    if (err) return next(err);
+    res.json(data);
+  });
 });
 
 // Return router
