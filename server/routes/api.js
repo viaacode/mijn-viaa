fc8fe1447bb5aad2ed466bb643023790a403cbd9
Tjen Wellens <(none)>
2016-07-06 16:18:07 +0200

    refactor api routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,46 +1,39 @@
 var express = require('express');
 var request = require('request');
+
 var router = express.Router();
 
+//region Magical Numbers
 // used in callback argument to return if successful
 // otherwise the error object/message is returned (not a falsy value)
 var SUCCESS = '';
-var NO_ORGANISATION = '';
 
-//region Dummy data
-var dummy = {
-  global: {
-    terabytes: 42,
-    items: 2471,
-    archive_growth: 446.12,
-    registration_growth: 443.5
-  },
-  organisations: createDummyOrganisationsStats()
-};
-
-function createDummyOrganisationsStats () {
-  var companies = [];
-  companies["VRT"] = {
-    terabytes: 33,
-    items: 234,
-    archive_growth: 3333.12,
-    registration_growth: 867.5
-  };
-  companies["plantentuin"] = {
-    terabytes: 11,
-    items: 64564,
-    archive_growth: 456.12,
-    registration_growth: 23.5
-  };
-  return companies;
-}
+// used to indicate that global stats of VIAA requested
+var VIAA_STATS = '';
 //endregion
 
+router.get('/stats/', function (req, res, next) {
+  fetchStats(VIAA_STATS, function (err, data) {
+    if (err) return next(err);
+    res.json(data);
+  });
+});
+
+// todo: auth
+router.get('/stats/organisation/:organisationId', function (req, res, next) {
+  var organisationId = req.params.organisationId;
+  fetchStats(organisationId, function (err, data) {
+    if (err) return next(err);
+    res.json(data);
+  });
+});
+
+//region Helper methods
 function fetchStats (organisation, callback) {
 
   var url = 'http://labs.viaa.be/api/v1/archived';
 
-  if (organisation !== NO_ORGANISATION) {
+  if (organisation !== VIAA_STATS) {
     url += '?tenant=' + organisation;
   }
 
@@ -62,22 +55,7 @@ function parseStats (inputObject) {
     registration_growth: 111.5
   };
 }
-
-router.get('/stats/', function (req, res, next) {
-  fetchStats(NO_ORGANISATION, function (err, data) {
-    if (err) return next(err);
-    res.json(data);
-  });
-});
-
-// todo: auth
-router.get('/stats/organisation/:organisationId', function (req, res, next) {
-  var organisationId = req.params.organisationId;
-  fetchStats(organisationId, function (err, data) {
-    if (err) return next(err);
-    res.json(data);
-  });
-});
+//endregion
 
 // Return router
 module.exports = router;
