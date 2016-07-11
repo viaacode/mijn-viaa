fce34398f4acf092e4b38ecffb19400e47c0166d
Tjen Wellens <(none)>
2016-07-11 08:46:55 +0200

    changed server log message

--- server/server.js
+++ server/server.js
@@ -2,5 +2,5 @@ var app = require('./app');
 
 // Start server
 app.listen(1337, function () {
-  console.log('--Eva-API available on port 1337');
+  console.log('--VIAA API available on port 1337');
 });
