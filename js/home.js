a4a252eac9380adcdef3a4128d08d23280bf6ceb
Laurens De Cock <laurens.decock@student.odisee.be>
2016-07-11 18:57:24 +0200

    Add error message for failed ajax call

--- js/home.js
+++ js/home.js
@@ -3,18 +3,21 @@ $(document).ready(function() {
     new Vue({
         el: '#home-app',
         data: {
-            stats: {  } // Initialize stats for binding
+            stats: { }, // Initialize stats for binding
+            errormsg: '' // Initialize error msg 
         },
         created: function() { // As soon as instance of Vue is created, do the ajax call and populate stats variable
             console.log("Ajax call");
-            var that = this;
-            $.ajax({url: "http://localhost:1337/api/stats", 
+            var that = this;    // !Scope -> within $.ajax(), 'this' will point to the ajax call
+            $.ajax({
+                url: "http://localhost:1337/api/stats", 
                 success: function(result){
                     console.log(result);
                     that.stats = result;
                 },
                 error: function(err) {
                     console.log(err);
+                    that.errormsg = err.status + ' - ' + err.statusText;
                 }            
             });
         }
