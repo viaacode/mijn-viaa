f5b5e571f1b231b1389630fd5da38881528198ef
Laurens De Cock <laurens.decock@student.odisee.be>
2016-07-11 12:00:47 +0200

    Made it nicer

--- js/services-app.js
+++ js/services-app.js
@@ -6,59 +6,59 @@ $(document).ready(function() {
         return (2 == 2);
     }
 
-    function getlistitems() {    
-        return  [
-            {
-                title:"FTP &ndash; Bestanden",
-                description:"Korte info Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum viverra malesuada. Sed auctor accumsan tempus. Nulla ac augue a odio porttitor porta. Phasellus venenatis ornare dolor, sed luctus justo mattis et. Donec tincidunt lacus eget, ipsum placerat pharetra. Nulla eget varius metus.",
-                link_info:"detailAms.html",
-                link_service:"ftp://ftp.viaa.be/",
-                img:"assets/ftp.svg",
-                img_classname:"service-icon-ftp",
-                alt:"FTP icon",
-                available: isAvailable(),
-                
-            },
-            {
-                title:"AMS &ndash; Registratie",
-                description:"Korte info Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum viverra malesuada. Sed auctor accumsan tempus. Nulla ac augue a odio porttitor porta. Phasellus venenatis ornare dolor, sed luctus justo mattis et. Donec tincidunt lacus eget, ipsum placerat pharetra. Nulla eget varius metus.",
-                link_info:"detailAms.html",
-                link_service:"http://registratie.viaa.be",
-                img:"assets/avo.svg",
-                img_classname:"service-icon-ams",
-                alt:"AMS icon",
-                available:false,
-            },
-            {
-                
-                title:"MAM &ndash; Archief",
-                description:"Korte info Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum viverra malesuada. Sed auctor accumsan tempus. Nulla ac augue a odio porttitor porta. Phasellus venenatis ornare dolor, sed luctus justo mattis et. Donec tincidunt lacus eget, ipsum placerat pharetra. Nulla eget varius metus.",
-                link_info:"detailMam.html",
-                link_service:"https://archief.viaa.be/",
-                img:"assets/mam.svg",
-                img_classname:"service-icon-mam",
-                alt:"MAM icon",
-                available: true,
-            },
-            {
-                
-                title:"DBS &ndash; Contracten",
-                description:"Korte info Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum viverra malesuada. Sed auctor accumsan tempus. Nulla ac augue a odio porttitor porta. Phasellus venenatis ornare dolor, sed luctus justo mattis et. Donec tincidunt lacus eget, ipsum placerat pharetra. Nulla eget varius metus.",
-                link_info:"detailDbs.html",
-                link_service:"https://oauth.viaa.be/user/login.jsp",
-                img:"assets/avo.svg",
-                img_classname:"service-icon-dbs",
-                alt:"DBS icon",
-            },
-        ];
-    }
-
-
-
     new Vue({
         el: '#services-app',
         data: {
-            listitems: getlistitems()
+            serviceslist: [
+                {
+                    title:"FTP &ndash; Bestanden",
+                    description:"Korte info Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum viverra malesuada. Sed auctor accumsan tempus. Nulla ac augue a odio porttitor porta. Phasellus venenatis ornare dolor, sed luctus justo mattis et. Donec tincidunt lacus eget, ipsum placerat pharetra. Nulla eget varius metus.",
+                    link_info:"detailAms.html",
+                    link_service:"ftp://ftp.viaa.be/",
+                    img:"assets/ftp.svg",
+                    img_classname:"service-icon-ftp",
+                    alt:"FTP icon",
+                    available: isAvailable(),
+                    
+                },
+                {
+                    title:"AMS &ndash; Registratie",
+                    description:"Korte info Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum viverra malesuada. Sed auctor accumsan tempus. Nulla ac augue a odio porttitor porta. Phasellus venenatis ornare dolor, sed luctus justo mattis et. Donec tincidunt lacus eget, ipsum placerat pharetra. Nulla eget varius metus.",
+                    link_info:"detailAms.html",
+                    link_service:"http://registratie.viaa.be",
+                    img:"assets/avo.svg",
+                    img_classname:"service-icon-ams",
+                    alt:"AMS icon",
+                    available:false,
+                },
+                {
+                    
+                    title:"MAM &ndash; Archief",
+                    description:"Korte info Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum viverra malesuada. Sed auctor accumsan tempus. Nulla ac augue a odio porttitor porta. Phasellus venenatis ornare dolor, sed luctus justo mattis et. Donec tincidunt lacus eget, ipsum placerat pharetra. Nulla eget varius metus.",
+                    link_info:"detailMam.html",
+                    link_service:"https://archief.viaa.be/",
+                    img:"assets/mam.svg",
+                    img_classname:"service-icon-mam",
+                    alt:"MAM icon",
+                    available: true,
+                },
+                {
+                    
+                    title:"DBS &ndash; Contracten",
+                    description:"Korte info Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum viverra malesuada. Sed auctor accumsan tempus. Nulla ac augue a odio porttitor porta. Phasellus venenatis ornare dolor, sed luctus justo mattis et. Donec tincidunt lacus eget, ipsum placerat pharetra. Nulla eget varius metus.",
+                    link_info:"detailDbs.html",
+                    link_service:"https://oauth.viaa.be/user/login.jsp",
+                    img:"assets/avo.svg",
+                    img_classname:"service-icon-dbs",
+                    alt:"DBS icon",
+                },
+            ]
+
+        },
+        methods: {
+            getlistitems: function() {    
+                    return ;
+                }
         }
     });
 
