022eb5cab874bdfcfb47ecdc0a8306c6e63b2d47
Laurens De Cock <laurens.decock@student.odisee.be>
2016-07-11 10:17:16 +0200

    Made template for service overview item

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
+
 $(document).ready(function() {
     new Vue({
-        el: '#app',
+        el: '#service-list',
+        components: {
+            'service-listitem': {
+
+                template: '#service-listitem-template',
+                props: ['title', 'description', 'link_info', 'link_service', 'icon_classname', 'img']
+            }  
+        },
         data: {
             message: 'Vue.js is working'
         }
