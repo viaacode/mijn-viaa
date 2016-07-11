aea39d1d234382dc2abf719b9eaafd2d1f30fa2b
Tjen Wellens <(none)>
2016-07-11 15:51:53 +0200

    ejs as template engine

--- server/routes/documentation.js
+++ server/routes/documentation.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var router = express.Router();
 var path = require("path");
+var ejs = require("ejs");
 
-router.get('/', function (req, res, next) {
-  res.sendFile(path.join(__dirname + '/documentation.html'));
-});
+var displayRoutes = [
+  {title: 'stats', url: '/api/stats'},
+  {title: 'services', url: '/api/services/MAM'},
+  {title: 'reports', url: '/api/reports/items-last-month'}
+];
 
-router.get('/api/', function (req, res, next) {
-  res.sendFile(path.join(__dirname + '/documentation.html'));
-});
+function showDocumentation (req, res, next) {
+  var data = {
+    routes: displayRoutes
+  };
+  ejs.renderFile(__dirname + '/documentation.ejs', data, {delimiter: "%"}, function (err, str) {
+    if (err) return next(err);
+    res.send(str);
+  });
+}
+
+router.get('/', showDocumentation);
+
+router.get('/api/', showDocumentation);
 
 // Return router
 module.exports = router;
