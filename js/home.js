735c80888580234fcb8d3899d3573ed094994a7a
Laurens De Cock <laurens.decock@student.odisee.be>
2016-07-12 11:25:22 +0200

    Resolved #5: Implement 1 hour caching for homepage

--- js/home.js
+++ js/home.js
@@ -1,25 +1,36 @@
 $(document).ready(function() {
-
     new Vue({
         el: '#home-app',
         data: {
             stats: '', // Initialize stats for binding
             errormsg: '' // Initialize error msg 
         },
-        created: function() { // As soon as instance of Vue is created, do the ajax call and populate stats variable
-            console.log("Ajax call");
-            var that = this;    // !Scope -> within $.ajax(), 'this' will point to the ajax call
-            $.ajax({
-                url: "http://localhost:1337/api/stats", 
-                success: function(result){
-                    console.log(result);
-                    that.stats = result;
-                },
-                error: function(err) {
-                    console.log(err);
-                    that.errormsg = err.status + ' - ' + err.statusText;
-                }            
-            });
+        created: function() { // As soon as instance of Vue is created, fill in the stats object
+            var nowMillis = new Date().getTime();
+            var cachedStats = JSON.parse(localStorage.getItem('stats'));
+            var backThenMillis = cachedStats.backThenMillis;
+            var refreshRate = 60 * 60 * 1000; // One hour
+            var lessThanAnHourAgo = backThenMillis !== null && ((nowMillis - backThenMillis) < refreshRate);
+
+            if(lessThanAnHourAgo) {
+                this.stats = cachedStats; 
+            } 
+            else {                
+                var that = this; // !Scope -> within $.ajax(), 'this' will point to the ajax call
+                $.ajax({
+                    url: "http://localhost:1337/api/stats", 
+                    success: function(result){
+                        // Save time to localstorage when this ajax call is made
+                        result.backThenMillis = nowMillis;  
+                        localStorage.setItem('stats', JSON.stringify(result));
+                        that.stats = result;
+                    },
+                    error: function(err) {
+                        console.log(err);
+                        that.errormsg = err.status + ' - ' + err.statusText;
+                    }            
+                });
+            }
         }
 
     });
