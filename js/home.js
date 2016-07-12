354ad567b5f8226ed7918f81006feb3a1b615241
Eva Jacobs <evajacobs22@gmail.com>
2016-07-12 17:22:00 +0200

    made home page responsive + styling

--- js/home.js
+++ js/home.js
@@ -4,11 +4,13 @@ $(document).ready(function() {
         el: '#home-app',
         data: {
             stats: '', // Initialize stats for binding
+            articles : '',
             errormsg: '' // Initialize error msg 
         },
         created: function() { // As soon as instance of Vue is created, do the ajax call and populate stats variable
             console.log("Ajax call");
             var that = this;    // !Scope -> within $.ajax(), 'this' will point to the ajax call
+            //fetch stats 
             $.ajax({
                 url: "http://localhost:1337/api/stats", 
                 success: function(result){
@@ -20,6 +22,19 @@ $(document).ready(function() {
                     that.errormsg = err.status + ' - ' + err.statusText;
                 }            
             });
+            //fetch articles
+            $.ajax({
+                url: "http://localhost:1337/api/services/MAM", 
+                success: function(result){
+                    console.log(result);
+                    that.articles = result.articles;
+                },
+                error: function(err) {
+                    console.log(err);
+                    that.errormsg = err.status + ' - ' + err.statusText;
+                }            
+            });
+
         }
 
     });
