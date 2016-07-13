ea396e8bcf4615dae6494fcc241ae48822642ca7
Eva Jacobs <evajacobs22@gmail.com>
2016-07-13 10:25:43 +0200

    made a bar chart with dummy data

--- js/reports-app.js
+++ js/reports-app.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
                 url: "http://localhost:1337/api/reports/items-last-month", 
                 success: function(result){
                     parseResults(result);
+                    parseBarResults(result);
                     console.log(result.data);
                     that.dataAPI = result;
                 },
@@ -32,6 +33,18 @@ function parseResults(result){
         parsedResult.push(obj);
     }
     drawChart(parsedResult, result);
+}
+
+function parseBarResults(result){
+    var parsedTimestamps = [];
+    var parsedValues = [];
+    for(var i = 0; i < result.data.length ; i++){
+        var timestamp = moment(result.data[i].timestamp).format('MM DD hh:ss');
+        var value = result.data[i].value;
+        parsedTimestamps.push(timestamp);
+        parsedValues.push(value);
+    }
+    drawBarChart(parsedValues, parsedTimestamps, result);
 }
 function drawChart(parsedResult,result){
     console.log(result);
@@ -73,5 +86,44 @@ function drawChart(parsedResult,result){
         }
     });
 }  
+
+function drawBarChart(parsedValues, parsedTimestamps, result){
+    var ctx2 = document.getElementById("myChart2");
+    var myChart2 = new Chart(ctx2, {
+        type: 'bar',
+        data: {
+            labels: parsedTimestamps,
+            datasets: [{
+                label: result.report,
+                data: parsedValues,
+                backgroundColor:'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255,99,132,1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                xAxes: [{
+                        type: "time",
+                        time: {
+                            format: 'MM DD',
+                            // round: 'day'
+                            tooltipFormat: 'll HH:mm'
+                        },
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Date'
+                        }
+                    }, ],
+                    yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'value'
+                        }
+                    }]
+            }
+        }
+});
+}
 
 });
