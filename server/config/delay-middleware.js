5e11fdcababe47a90f12adf6df8962b466c60aa9
Tjen Wellens <(none)>
2016-07-14 21:10:07 +0200

    set delay in config file to be between 2 and 5 seconds

--- server/config/delay-middleware.js
+++ server/config/delay-middleware.js
@@ -1,12 +1,12 @@
 var _ = require('underscore');
 
-module.exports = function (req, res, next) {
-  var min = 3; //seconds
-  var max = 10; //seconds
-  var duration = Math.floor(Math.random() * (max - min) + min);
-  duration *= 1000;
+module.exports = function (config) {
+  return function (req, res, next) {
+    var min = config.min || 3; //seconds
+    var max = config.max || 10; //seconds
+    var duration = Math.floor(Math.random() * (max - min) + min);
+    duration *= 1000;
 
-  _.delay(function () {
-    next();
-  }, duration);
+    _.delay(next, duration);
+  };
 };
