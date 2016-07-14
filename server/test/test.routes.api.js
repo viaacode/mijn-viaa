240c3f56f593c5af3fb7ba962e0c358dbaf4ed0e
Tjen Wellens <(none)>
2016-07-15 00:27:46 +0200

    test stats

--- server/test/test.routes.api.js
+++ server/test/test.routes.api.js
@@ -1,11 +1,13 @@
 var express = require('express');
-var request = require('supertest');
+var supertest = require('supertest');
 
 var apiRoutes = require('../routes/api');
 
 var FAKE_REQUEST = {
-  success: function request (url, callback) {
-    callback(null, {statusCode: 200}, {foo: "bar"});
+  success: function createRequestMockWhichReturns (body) {
+    return function requestMock (url, callback) {
+      callback(null, {statusCode: 200}, body);
+    }
   }
 };
 
@@ -13,19 +15,35 @@ var FAKE_REQUEST = {
 describe('routs/api', function () {
   var app;
 
-  beforeEach(function() {
+  beforeEach(function () {
     app = express();
   });
 
   it('/mule-test should pass json from request without changing it', function (done) {
     var config = {};
+    var request = FAKE_REQUEST.success({foo: "bar"});
 
-    apiRoutes(app, config, FAKE_REQUEST.success);
+    apiRoutes(app, config, request);
 
-    request(app)
+    supertest(app)
       .get('/api/mule-test')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200, '{"foo":"bar"}', done);
+  });
+
+  it('/stats should format requested data', function (done) {
+    var config = {};
+    var input = '{"request":{"url":"http://labs.viaa.be/api/v1/archived?tenant=VRT","timestamp":"2016-07-15 00:02:27 +0200","tenant":"VRT","version":"v1","status":"archived"},"response":{"data":{"archived":{"all":8073,"bytes":71661141382754.0,"gigabytes":"66739.64","terabytes":"65.18","petabytes":"0.06"}}}}';
+    var expected = '{"terabytes":"65.18","items":8073,"archive_growth":111.12,"registration_growth":111.5}';
+    var request = FAKE_REQUEST.success(input);
+
+    apiRoutes(app, config, request);
+
+    supertest(app)
+      .get('/api/stats')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, expected, done);
   });
 });
