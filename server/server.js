aaf73f764899584a5349cbdfbfde16d061a7556e
Tjen Wellens <(none)>
2016-07-18 11:45:48 +0200

    split server.js into configuration and running for testing

--- server/server.js
+++ server/server.js
@@ -1,67 +1,9 @@
-var express = require('express');
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var passport = require('passport');
-var session = require('express-session');
-var request = require('request');
-
-var allowCors = require('./config/cors');
-var authMiddleware = require('./config/authentication-middleware');
-var delayMiddleware = require('./config/delay-middleware');
+var configEnvironments = require('./config/config');
 
 var env = process.env.NODE_ENV || 'development';
-var config = require('./config/config')(env);
+var config = configEnvironments(env);
 
-// Express
-var app = express();
-
-app.set('port', config.app.port);
-// app.set('views', __dirname + '/app/views');
-app.set('view engine', 'ejs');
-app.use(allowCors);
-app.use(morgan('dev'));
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(session({
-  resave: true,
-  saveUninitialized: true,
-  secret: config.app.sessionSecret
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Routes
-require('./routes/documentation')(app, config);
-
-// Routes for API
-var apiRouter = express.Router();
-if (config.passport) {
-  require('./routes/authentication')(app, config, passport);
-  apiRouter.use(authMiddleware.errorCode);
-}
-
-apiRouter.use(delayMiddleware(config));
-
-require('./routes/api')(apiRouter, config, request);
-app.use('/', apiRouter);
-
-// Error handling
-app.use(function (err, req, res, next) {
-  //Only print stacktrace when in a dev environment
-  var stackTrace = {};
-  if (config.showErrors) {
-    stackTrace = err;
-  }
-
-  console.error(err);
-  res.status(500).send({
-    status: 500,
-    message: err.message,
-    error: stackTrace,
-    type: 'internal'
-  });
-});
+var app = require('./app')(config);
 
 // Start server
 app.listen(config.app.port, function () {
