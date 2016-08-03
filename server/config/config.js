8931dba163c44c16af05bdbac807adfc5db7fada
Brecht Van de Vyvere <brecht.vandevyvere@viaa.be>
2016-08-03 14:48:44 +0200

    Add authentication back in the game. Set QAS env default with port 80

--- server/config/config.js
+++ server/config/config.js
@@ -101,7 +101,7 @@ var qas = {
   app: {
     // used in console to tell which app is started
     name: 'mijn.VIAA',
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 80,
     sessionSecret: process.env.SESSION_SECRET || 'mijnVIAAetc'
   },
   // show extended error messages in api calls
